63a12f398c7a7421a357a5bdb8e3d05b15fae2f1
Pavel <55947748+PavelYakovlev1984@users.noreply.github.com>
2022-06-22 14:10:16 +0300

    past

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Pavel Yakovlev <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <div><Header/></div>
+    <Technologies/>
     </div>
+  );
+}
+
+const Technologies = () => {
+  return (
+    <ul>
+    <li>CSS</li>
+    <li>html</li>
+    <li>js</li>
+    <li>react</li></ul>
+  )
+}
+
+
+const Header = () => {
+  return (
+   <div>
+   <a href="#">Home</a><a href="#">News Feed</a><a href="#">Messages</a>
+   </div>
   );
 }
 
